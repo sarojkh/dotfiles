9d999e7b8e7474e1e2b85ff8a69d6eb82e5b4e8b
sarojkh <alt.sarojk@gmail.com>
2015-12-04 19:56:50 +0545

    Symlinking added for different dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,12 @@
-#/bin/bash
+: '
+TODO:
+	-Echo all the steps
+'
+dotfile_root="/Users/sarojk/Google Drive/dotfiles"
 
-echo "Howdy"
+ln -s "$dotfile_root/vim/.vimrc" ~/.vimrc
+ln -s "$dotfile_root/vim/.vim/" ~/.vim
+
+ln -s "$dotfile_root/bash/.profile" ~/.profile
+
+ln -s "$dotfile_root/cron/.cron" ~/.cron
