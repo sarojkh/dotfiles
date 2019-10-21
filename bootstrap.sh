47ee1770bcbab13cd458d40029984003cce213ec
sarojkh <alt.sarojk@gmail.com>
2019-10-21 22:54:01 +0545

    Remove stale way of doing things.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,6 @@ TODO:
 '
 dotfile_root=~/"git/personal/dotfiles"
 
-# Create symlinks for dotfiles
-ln -s "$dotfile_root/vim/.vimrc" ~/.vimrc
-
-# Install Vundle.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-ln -s "$dotfile_root/bash/.bash_profile" ~/.bash_profile
-ln -s "$dotfile_root/bash/.bashrc" ~/.bashrc
 ln -s "$dotfile_root/bash/.aliases" ~/.aliases
-ln -s "$dotfile_root/bash/.bash_prompt" ~/.bash_prompt
 ln -s "$dotfile_root/bash/.bash_functions" ~/.bash_functions
 
-ln -s "$dotfile_root/cron/.cron" ~/.cron
-
-# Create folders for swap, backup and undo files for Vim
-mkdir ~/.vim/backup
-mkdir ~/.vim/swap
-mkdir ~/.vim/undo
-
-# Execute ~/.bash_profile
-source ~/.bash_profile
